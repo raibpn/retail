0193a56d6f24e9e9d43ecbff018b877172c84964
raibpn <raibpn@users.noreply.github.com>
2023-02-02 20:59:32 +0100

    landing page designing

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -6,114 +6,111 @@ import Image from "next/image";
 
 const Landing = () => {
   return (
-    <div className="m-4 flex items-center gap-4">
-      <div
-        className="relative w-1/3 rounded-lg border border-gray-200 bg-white shadow
-       dark:border-gray-700 dark:bg-gray-800 md:h-full md:min-w-[360px] md:hover:scale-105"
-      >
-        <img
-          className="md:w-58 md:h-65 max-h-md h-full w-full rounded-sm object-fill md:rounded"
-          src="/asset/head1.jpg"
-          alt="image"
-        />
-
-        <div className="p-5">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
+    <div className="m-4 flex items-center justify-center">
+      <div className="grid grid-cols-1 gap-5 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+        <div className="h-[410px] max-w-[420px] overflow-hidden rounded shadow-lg md:h-[610px]">
+          <img
+            className="h-[160px] w-full object-fill md:h-[380px]"
+            src="/asset/head1.jpg"
+            alt="Mountain"
+          />
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">Mountain</div>
+            <p className="text-base text-gray-700">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, Nonea! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div className="h-5 px-6 pt-4 pb-2 md:h-20 lg:h-20">
+            <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+              #photography
+            </span>
+            <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+              #travel
+            </span>
+            <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+              #winter
+            </span>
+          </div>
         </div>
-      </div>
-
-      <div
-        className="relative w-1/3 rounded-lg border border-gray-200 bg-white shadow
-       dark:border-gray-700 dark:bg-gray-800 md:h-full md:min-w-[360px] md:hover:scale-105"
-      >
-        <img
-          className="md:w-58 md:h-65 max-h-md h-full w-full rounded-sm object-fill md:rounded"
-          src="/asset/head1.jpg"
-          alt="image"
-        />
-
-        <div className="p-5">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
+        <div className="h-[410px] max-w-[420px] overflow-hidden rounded shadow-lg md:h-[610px]">
+          <img
+            className="h-[160px] w-full object-fill md:h-[380px]"
+            src="/asset/head1.jpg"
+            alt="Mountain"
+          />
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">Mountain</div>
+            <p className="text-base text-gray-700">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, Nonea! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div className="h-5 px-6 pt-4 pb-2 md:h-20 lg:h-20">
+            <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+              #photography
+            </span>
+            <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+              #travel
+            </span>
+            <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+              #winter
+            </span>
+          </div>
         </div>
-      </div>
-
-      <div
-        className="relative w-1/3 rounded-lg border border-gray-200 bg-white shadow
-       dark:border-gray-700 dark:bg-gray-800 md:h-full md:min-w-[360px] md:hover:scale-105"
-      >
-        <img
-          className="md:w-58 md:h-65 max-h-md h-full w-full rounded-sm object-fill md:rounded"
-          src="/asset/head1.jpg"
-          alt="image"
-        />
-
-        <div className="p-5">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
+        <div className="h-[410px] max-w-[420px] overflow-hidden rounded shadow-lg md:h-[610px]">
+          <img
+            className="h-[160px] w-full object-fill md:h-[380px]"
+            src="/asset/head1.jpg"
+            alt="Mountain"
+          />
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">Mountain</div>
+            <p className="text-base text-gray-700">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, Nonea! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div className="h-5 px-6 pt-4 pb-2 md:h-20 lg:h-20">
+            <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+              #photography
+            </span>
+            <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+              #travel
+            </span>
+            <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+              #winter
+            </span>
+          </div>
         </div>
-      </div>
-
-      <div
-        className="relative w-1/3 rounded-lg border border-gray-200 bg-white shadow
-       dark:border-gray-700 dark:bg-gray-800 md:h-full md:min-w-[360px] md:hover:scale-105"
-      >
-        <img
-          className="md:w-58 md:h-65 max-h-md h-full w-full rounded-sm object-fill md:rounded"
-          src="/asset/head1.jpg"
-          alt="image"
-        />
-
-        <div className="p-5">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-        </div>
-      </div>
-
-      <div
-        className="relative w-1/3 rounded-lg border border-gray-200 bg-white shadow
-       dark:border-gray-700 dark:bg-gray-800 md:h-full md:min-w-[360px] md:hover:scale-105"
-      >
-        <img
-          className="md:w-58 md:h-65 max-h-md h-full w-full rounded-sm object-fill md:rounded"
-          src="/asset/head1.jpg"
-          alt="image"
-        />
-
-        <div className="p-5">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
+        <div className="h-[410px] max-w-[420px] overflow-hidden rounded shadow-lg md:h-[610px]">
+          <img
+            className="h-[160px] w-full object-fill md:h-[380px]"
+            src="/asset/head1.jpg"
+            alt="Mountain"
+          />
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">Mountain</div>
+            <p className="text-base text-gray-700">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, Nonea! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div className="h-5 px-6 pt-4 pb-2 md:h-20 lg:h-20">
+            <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+              #photography
+            </span>
+            <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+              #travel
+            </span>
+            <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+              #winter
+            </span>
+          </div>
         </div>
       </div>
     </div>
