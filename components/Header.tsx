015c0287d5761352560b92a93be988c4a5a39c0e
raibpn <raibpn@users.noreply.github.com>
2023-02-02 16:39:21 +0100

    fonts added

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,10 +12,13 @@ const Header = () => {
   const session = false;
   return (
     <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-black">
-      <div className="flex items-center justify-start px-1 md:w-1/5">
+      <div className="flex items-center justify-start px-4 pt-1 md:w-1/5">
         <Link href="/">
-          <div className="relative h-10 w-10 cursor-pointer transition hover:opacity-100">
+          <div className="relative flex h-10 w-12 cursor-pointer items-center justify-between transition hover:opacity-100">
             <Image src={logo} alt="image" fill className="object-cover" />
+          </div>
+          <div className="relative float-right text-white">
+            <p>BuyKin</p>
           </div>
         </Link>
       </div>
