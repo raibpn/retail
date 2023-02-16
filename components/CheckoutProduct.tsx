40da829997f42bf2bda7f1033baa29d87b368c49
raibpn <raibpn@users.noreply.github.com>
2023-02-16 01:01:36 +0100

    accumulated price

--- components/CheckoutProduct.tsx
+++ components/CheckoutProduct.tsx
@@ -17,10 +17,6 @@ const CheckoutProduct = ({ id, items }: Props) => {
   const [price, setTotalPrice] = useState<number>();
   const dispatch = useDispatch();
 
-  const getTotalPrice = (items: Product[]): number => {
-    return items.reduce((total, item) => item.price + total, 0);
-  };
-
   const removeItemFromBasket = () => {
     dispatch(removeFromBasket({ id }));
 
@@ -33,7 +29,7 @@ const CheckoutProduct = ({ id, items }: Props) => {
     <div className="flex flex-col gap-x-4 border-b border-gray-300 p-4 pb-5 lg:flex-row lg:items-center">
       <div className="relative h-44 w-44">
         <Image
-          className=""
+          className="p-2 transition delay-150 ease-in-out hover:scale-125"
           src={`${items[0].image}`}
           alt="image"
           fill
@@ -43,7 +39,7 @@ const CheckoutProduct = ({ id, items }: Props) => {
 
       <div className="flex flex-1 items-end lg:items-center">
         <div className="flex-1 space-y-4">
-          <div className="flex flex-col gap-x-8 text-xl lg:flex-row lg:text-2xl">
+          <div className="flex flex-col gap-x-8 p-4 text-xl lg:flex-row lg:text-2xl">
             <h4 className="font-semibold lg:w-96">{items[0].title}</h4>
             <p className="flex items-end gap-x-1 font-semibold">
               {items.length}
@@ -51,7 +47,7 @@ const CheckoutProduct = ({ id, items }: Props) => {
             </p>
           </div>
 
-          <p className="flex cursor-pointer items-end text-blue-500 hover:underline">
+          <p className="flex max-w-fit cursor-pointer items-end text-blue-500 hover:underline">
             Show product details
             <ChevronDownIcon className="h-6 w-6" />
           </p>
@@ -59,15 +55,12 @@ const CheckoutProduct = ({ id, items }: Props) => {
         <div className="flex flex-col items-end space-y-4">
           <h4 className="text-xl font-semibold lg:text-2xl">
             <Currency
-              quantity={items.reduce(
-                (total, item) => total + item.price * item.id.length,
-                0
-              )}
+              quantity={items?.reduce((total, item) => total + item.price, 0)}
               currency="DKK"
             />
           </h4>
           <button
-            // onClick={removeItemFromBasket}
+            onClick={removeItemFromBasket}
             className="text-blue-500 hover:underline"
           >
             Remove
